d2964d9dca75fab5c2f3b2d78893023967cd98b4
andrewhe6 <andrewhe9264@gmail.com>
2025-11-14 01:00:56 -1000

    Integrate frontend pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './input.css'
 
-import StudyAdvisorLanding from '../page-landing.tsx'
-import UHPathfinderAI from '../page-uhresults.tsx'
+import StudyAdvisorLanding from './pages/page-landing.tsx'
+import UHPathfinderAI from './pages/page-uhresults.tsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StudyAdvisorLanding />} />
-        <Route path="/uh" element={<UHPathfinderAI />} />
+        <Route path="/pathway" element={<UHPathfinderAI />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>

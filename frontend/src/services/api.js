b7fc962396efc0576c2b4e00a181176989862511
andrewhe6 <andrewhe9264@gmail.com>
2025-11-15 18:48:11 -1000

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:5000/api";
+// const API_URL = "http://127.0.0.1:5000/api";
+const API_URL = "https://lokahi-lions.onrender.com/api";
 
 export async function fetchHello() {
   const response = await fetch(`${API_URL}/hello`);

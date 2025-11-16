b0344d0028a6343594fc0c07b84cb204f874be82
andrewhe6 <andrewhe9264@gmail.com>
2025-11-15 18:27:25 -1000

    Update CORS url

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(api_bp)
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "http://localhost:5000", "http://127.0.0.1:5000", "http://127.0.0.1:5173"]}})
+    CORS(app, resources={r"/*": {"origins": ["https://uh-pathfinder.vercel.app/", "https://uh-pathfinder-lokahi-lions.vercel.app/"]}})
 
     return app
 
